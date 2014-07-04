2b93d5a2c773b0ff835d01afb3c3c139fb5602d3
TJ Ferry <tjferry@TJs-MacBook-Air.local>
2014-07-04 12:37:20 -0400

    took sebastians suggestions

--- cloud_maker.py
+++ cloud_maker.py
@@ -1,31 +1,30 @@
 import Image, ImageDraw, random, scene
 
-def generate_shapes(num_circles = random.randint(4, 5)):
+def generate_shapes(num_circles):
     shapes = []
     for i in xrange(num_circles):
-        x = i * 20 - ((num_circles/2)*30)
-        y = (random.random()-0.5) * 30
-        x += 60  # this is a hack!
-        y += 16  # this is a hack!
+        x = (i * 20 - ((num_circles/2)*30))+90
+        y = ((random.random()-0.5) * 30)+15
         rad = random.randint(50, 100)
         shapes.append([x, y, rad])
     return shapes
 
 def cloud_maker():
-    num_circles = random.randint(4, 5)
-    #image_size = ((num_circles + 1) * 30, 60)
-    image_size = (180, 128)
-    theImage = Image.new('RGBA', image_size) #, 'pink')
+    num_circles = random.randint(5, 6)
+    image_size = (220, 140)
+    theImage = Image.new('RGBA', image_size)
     draw = ImageDraw.Draw(theImage)
-    
+
     circles = generate_shapes(num_circles)
     for i in circles:
-        bbox = (i[0], i[1] - 5, i[2], i[2])
+        r = i[2]
+        bbox = (i[0], 40-i[1], i[0]+r, 40-i[1]+r)
         draw.ellipse(bbox, fill='rgb(90%,90%,90%)')
     for i in circles:
-        bbox = (i[0], i[1] + 5, i[2], i[2])
+        r = i[2]
+        bbox = (i[0], 40-i[1]-10, i[0]+r, 40-i[1]+r-10)
         draw.ellipse(bbox, fill='white')
-    
+
     del draw
     return theImage
 
@@ -38,21 +37,22 @@ class Cloud(scene.Layer):
         self.image = scene.load_pil_image(cloud_image)
 
 class MyScene(scene.Scene):
-    def __init__(self):
-        scene.run(self)
 
     def setup(self):
         self.cloud = Cloud(self)
-        self.cloud.frame.x = 0
-        self.cloud.frame.y = self.bounds.h * 0.8
+        self.cloud.frame.x = self.bounds.w * 0.5
+        self.cloud.frame.y = self.bounds.h*0.8
 
     def draw(self):
         scene.background(0.40, 0.80, 1.00)
         self.root_layer.update(self.dt)
         self.root_layer.draw()
-        self.cloud.frame.x += 1
-        if not self.bounds.intersects(self.cloud.frame):
-            del self.cloud  # whack the old cloud
-            self.setup()    # and create a new one
+        scene.rect(*self.cloud.frame)
 
-MyScene()
+    def touch_began(self, touch):
+        self.root_layer.remove_layer(self.cloud)
+        self.cloud = Cloud(self)
+        self.cloud.frame.x = self.bounds.w * 0.5
+        self.cloud.frame.y = self.bounds.h*0.8
+
+scene.run(MyScene())
