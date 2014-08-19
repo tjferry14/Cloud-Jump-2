eb8fd3c7285be89f862713a970c4ace0c52803e8
TJ Ferry <tjferry@TJs-MacBook-Air.local>
2014-08-19 15:16:14 -0400

    added the other characters

--- ui-menu.py
+++ ui-menu.py
@@ -1,8 +1,9 @@
+# this is meant to be viewed in landscape mode on an iPad
 import ui
 
 def play_game(sender):
 	ui.close_all()
-
+	
 def change_character(sender):
 		v = ui.View()
 		v.background_color = (0.40, 0.80, 1.00)
@@ -23,6 +24,18 @@ def change_character(sender):
 		char3 = ui.Button(frame=(340, 160, 100, 100), 
 		                   image=ui.Image.named('Guardsman'))
 		v.add_subview(char3)
+		
+		char4 = ui.Button(frame=(495, 160, 100, 100), 
+		                   image=ui.Image.named('Hamster_Face'))
+		v.add_subview(char4)
+		
+		char5 = ui.Button(frame=(640, 160, 100, 100), 
+		                   image=ui.Image.named('Mouse_Face'))
+		v.add_subview(char5)
+		
+		char6 = ui.Button(frame=(795, 160, 100, 100), 
+		                   image=ui.Image.named('Man'))
+		v.add_subview(char6)
 		
 		v.present(style='full_screen', hide_title_bar=True)
 
