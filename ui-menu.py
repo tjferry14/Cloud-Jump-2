63b06bf063b663d09ffb0d31359592b16cb9710e
TJ Ferry <tjferry@TJs-MacBook-Air.local>
2014-08-19 14:59:20 -0400

    Changed image views to buttons
    Although they currently do not have actions

--- ui-menu.py
+++ ui-menu.py
@@ -1,19 +1,29 @@
 import ui
-import os
-images_root = os.path.expanduser('~/Documents/Images')
 
 def play_game(sender):
 	ui.close_all()
-	
+
 def change_character(sender):
 		v = ui.View()
-		v.background_color = (0.40, 0.80, 1.00)		
-		image_name = 'Banana'
-		image_path = os.path.join(images_root, image_name + '.png')
-		char1 = ui.ImageView()
-		char1.image = ui.Image.named(image_path)
-		char1.frame = (40, 600, 100, 100)	
+		v.background_color = (0.40, 0.80, 1.00)
+		header = ui.Label(frame = (200, 19.5, 700, 116.5))
+		header.text_color = (1.00, 1.00, 1.00)
+		header.text = 'Select A Character'
+		header.font = ('AvenirNext-Heavy', 70)
+		v.add_subview(header)
+		
+		char1 = ui.Button(frame=(40, 160, 100, 100), 
+		                   image=ui.Image.named('Boy'))
 		v.add_subview(char1)
+		
+		char2 = ui.Button(frame=(195, 160, 100, 100), 
+		                   image=ui.Image.named('Girl'))
+		v.add_subview(char2)
+		
+		char3 = ui.Button(frame=(340, 160, 100, 100), 
+		                   image=ui.Image.named('Guardsman'))
+		v.add_subview(char3)
+		
 		v.present(style='full_screen', hide_title_bar=True)
 
 v = ui.load_view('ui-menu')
