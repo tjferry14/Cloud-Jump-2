83b91c5771f5b6c8fa5003f20b601b8ae831f551
cclauss <cclauss@bluewin.ch>
2014-08-21 08:23:05 +0200

    Update ui-menu.py

--- ui-menu.py
+++ ui-menu.py
@@ -1,6 +1,8 @@
 # this is meant to be viewed in landscape mode on an iPad
 import ui
 
+characters = 'Boy Girl Guardsman Hamster_Face Mouse_Face Man'.split()
+
 def play_game(sender):
     ui.close_all()
 
@@ -11,22 +13,22 @@ def make_header():
     header.font = ('AvenirNext-Heavy', 70)
     return header
 
-def make_button(x, y, image_name = 'Boy'):
-    return ui.Button(frame=(x, y, 100, 100), image=ui.Image.named(image_name))
+def character_tapped(sender):
+    print('The user wants to be: ' + sender.name)
+
+def make_button(x, image_name = 'Boy'):
+    button = ui.Button(name=image_name,
+                       frame=(x, 160, 128, 128),
+                       image=ui.Image.named(image_name))
+    button.action=character_tapped
+    return button
 
 def change_character(sender):
     v = ui.View()
     v.background_color = (0.40, 0.80, 1.00)
     v.add_subview(make_header())
-    characters = [ make_button(40, 160, 'Boy'),
-                   make_button(195, 160, 'Girl'),
-                   make_button(340, 160, 'Guardsman'),
-                   make_button(495, 160, 'Hamster_Face'),
-                   make_button(640, 160, 'Mouse_Face'),
-                   make_button(795, 160, 'Man') ]
-    for character in characters:
-        v.add_subview(character)
-
+    for i, character in enumerate(characters):
+        v.add_subview(make_button(40 + i * 155, character))
     v.present(style='full_screen', hide_title_bar=True)
 
 v = ui.load_view('ui-menu')
